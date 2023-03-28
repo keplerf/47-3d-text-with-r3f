de0abf87f66cfd81212738252f74f6ed46f36e1f
Kepler Fontenele <keplerf@criticalmass.com>
2023-03-28 12:07:23 -0700

    init

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,19 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls, Text3D, Center } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <Perf position="top-left" />
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
-
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+      <Center>
+        <Text3D font="./fonts/Roboto Condensed_Regular.json">
+          Web Rocks
+          <meshNormalMaterial />
+        </Text3D>
+      </Center>
     </>
+  );
 }
