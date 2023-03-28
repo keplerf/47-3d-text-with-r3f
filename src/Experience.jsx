8b0581710422bda9a88ce4d2a6a6a9749fbccf4a
Kepler Fontenele <keplerf@criticalmass.com>
2023-03-28 12:35:45 -0700

    init

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,17 @@
-import { OrbitControls, Text3D, Center } from "@react-three/drei";
+import {
+  OrbitControls,
+  Text3D,
+  Center,
+  useMatcapTexture,
+} from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 export default function Experience() {
+  const matcapTexture = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+
+  const colorMap = useLoader(TextureLoader, "mat.jpg");
   return (
     <>
       <Perf position="top-left" />
@@ -9,9 +19,19 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <Center>
-        <Text3D font="./fonts/Roboto Condensed_Regular.json">
+        <Text3D
+          font="./fonts/Roboto Condensed_Regular.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
           Web Rocks
-          <meshNormalMaterial />
+          <meshMatcapMaterial matcap={colorMap} />
         </Text3D>
       </Center>
     </>
